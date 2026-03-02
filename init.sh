8d2549330555a3362145e1c005ad599c38063a1a
TanakaUkai <71815634+Qiuyc18@users.noreply.github.com>
2026-03-02 12:00:19 +0800

    Update init.sh to source ble.sh in .bashrc
    Add source command for ble.sh to .bashrc if not installed

--- init.sh
+++ init.sh
@@ -7,6 +7,7 @@ echo "==== 1. 安装 ble.sh ===="
 if [ ! -d "$HOME/.local/share/blesh" ]; then
   curl -L https://github.com/akinomyoga/ble.sh/releases/download/nightly/ble-nightly.tar.xz | tar xJf -
   bash ble-nightly/ble.sh --install ~/.local/share
+  echo 'source -- ~/.local/share/blesh/ble.sh' >> ~/.bashrc
   rm -rf ble-nightly
 else
   echo "ble.sh 已存在，跳过"
